ad23edba383c5d880e26d166412ae8d83d4c5efe
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-08 23:12:33 +0900

    feat: CRA todoApp

---
+++ todoapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>할 일 목록</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
