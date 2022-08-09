65c9b30f30b9770bab25f628d5748c7b38a180f5
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-09 12:12:13 +0900

    feat: 드래그앤 드랍 구현

--- react-beautiful-dnd/src/App.js
+++ react-beautiful-dnd/src/App.js
@@ -6,13 +6,50 @@ import Column from "./Column";
 import { DragDropContext } from "react-beautiful-dnd";
 function App() {
   const [state, setState] = useState(initialData);
-  const onDragEnd = (result) => {};
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+
+    console.log(result);
+    // 목적지가 없는 경우 (단순에러일듯)
+    if (!destination) {
+      return;
+    }
+
+    // 드래그했지만 드롭을 제자리에 한 경우
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    const column = state.columns[source.droppableId];
+    console.log(column);
+    const newTaskIds = [...column.taskIds];
+    console.log(newTaskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    setState(newState);
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       {state.columnOrder.map((columnId) => {
         const column = state.columns[columnId];
         const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
-        console.log(tasks);
+
         return <Column key={column.id} column={column} tasks={tasks} />;
       })}
     </DragDropContext>
