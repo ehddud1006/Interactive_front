3337d4041401aa1f1c8b27d455f6bcef0df2bdbd
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-09 12:40:44 +0900

    feat: 드래그앤 드롭시 시작할때 종료될떄 style

--- react-beautiful-dnd/src/App.js
+++ react-beautiful-dnd/src/App.js
@@ -6,7 +6,23 @@ import Column from "./Column";
 import { DragDropContext } from "react-beautiful-dnd";
 function App() {
   const [state, setState] = useState(initialData);
+
+  const onDragStart = () => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.2 ease";
+  };
+
+  const onDragUpdate = (update) => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(state.tasks).length
+      : 0;
+
+    document.body.style.backgroundColor = `rgba(153,141,217,${opacity})`;
+  };
   const onDragEnd = (result) => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
     const { destination, source, draggableId } = result;
 
     console.log(result);
@@ -53,7 +69,11 @@ function App() {
     setState(newState);
   };
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext
+      onDragStart={onDragStart}
+      onDragUpdate={onDragUpdate}
+      onDragEnd={onDragEnd}
+    >
       {state.columnOrder.map((columnId) => {
         const column = state.columns[columnId];
         const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
