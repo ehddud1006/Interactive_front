cdc5d6d0d19a6362b25c1af168512baec21f8355
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-09 16:33:29 +0900

    드래그앤 드롭 추가

--- todoapp/src/components/Lists.js
+++ todoapp/src/components/Lists.js
@@ -2,23 +2,43 @@ import React from "react";
 
 import styled from "styled-components";
 import { TodoText } from "./style";
+import { Droppable } from "react-beautiful-dnd";
+import { Draggable } from "react-beautiful-dnd";
+const Container = styled.div``;
+const TodoList = styled.div``;
 const Lists = ({ data, onChangeCompleted, deleteTodoListData }) => {
   return (
-    <div>
-      {data.map((el) => {
-        return (
-          <TodoText key={el.id} completed={el.completed}>
-            <input
-              onChange={() => onChangeCompleted(el.id)}
-              type="checkbox"
-              defaultChecked={false}
-            ></input>
-            <div>{el.text}</div>
-            <button onClick={() => deleteTodoListData(el.id)}>x</button>
-          </TodoText>
-        );
-      })}
-    </div>
+    <Droppable droppableId={"drop1"}>
+      {(provided) => (
+        <TodoList {...provided.droppableProps} ref={provided.innerRef}>
+          {data.map((el, index) => {
+            return (
+              <Draggable key={el.id} draggableId={el.id} index={index}>
+                {(provided) => (
+                  <Container
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    <TodoText key={el.id} completed={el.completed}>
+                      <input
+                        onChange={() => onChangeCompleted(el.id)}
+                        type="checkbox"
+                        defaultChecked={false}
+                      ></input>
+                      <div>{el.text}</div>
+                      <button onClick={() => deleteTodoListData(el.id)}>
+                        x
+                      </button>
+                    </TodoText>
+                  </Container>
+                )}
+              </Draggable>
+            );
+          })}
+        </TodoList>
+      )}
+    </Droppable>
   );
 };
 
