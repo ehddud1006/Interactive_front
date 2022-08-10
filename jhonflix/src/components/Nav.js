b29c38e39a67e8094a377b8a10c5ad09b26171f3
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-10 23:54:38 +0900

    [Jhonflix] feat: nav에 search bar 추가 구현

--- jhonflix/src/components/Nav.js
+++ jhonflix/src/components/Nav.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 const Nav = () => {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
   useEffect(() => {
     window.addEventListener("scroll", () => {
       // console.log("window.scrollY", window.scrollY);
@@ -16,6 +19,12 @@ const Nav = () => {
       window.removeEventListener("scroll", () => {});
     };
   }, []);
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <nav className={`nav ${show && "nav__black"} `}>
       <img
@@ -23,6 +32,13 @@ const Nav = () => {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         onClick={() => window.location.reload()}
+      />
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
       />
       <img
         alt="User logged"
