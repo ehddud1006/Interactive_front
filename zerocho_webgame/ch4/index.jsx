bd4188e186fe0306ba5b12bc978f490ef2ff4791
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-08-21 10:41:00 +0000

    4장

---
+++ zerocho_webgame/ch4/index.jsx
@@ -0,0 +1,6 @@
+const React = require("react");
+const ReactDom = require("react-dom");
+
+import NumberBaseball from "./ResponseCheckClass";
+
+ReactDom.render(<NumberBaseball />, document.querySelector("#root"));
